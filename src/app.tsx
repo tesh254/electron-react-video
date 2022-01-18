4cfc4ee303ccb81b484a68896a70d3aa3e0c0dd9
wachira_Dev <ewachira254@gmail.com>
2022-01-18 23:45:24 +0300

    @app/feature: show video after asking for permission

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC<{}> = () => {
-    return (
-        <section>
-            Hello this is react running on electron
-        </section>
-    )
-}
+  const [canShowVideo, setCanShowVideo] = React.useState(false);
+
+  const [toggleVideo, setToggleVideo] = React.useState(false);
+
+  function showVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: true })
+      .then((res) => {
+        setCanShowVideo(true);
+        setToggleVideo(true);
+        // @ts-ignore
+        document.getElementById("video").srcObject = res;
+      })
+      .catch((err) => {
+        setCanShowVideo(false);
+        alert("Could not access webcam");
+      });
+  }
+
+  React.useEffect(() => {}, []);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          if (toggleVideo) {
+            setToggleVideo(false);
+          } else {
+            showVideo();
+          }
+        }}
+      >
+        {toggleVideo ? "Hide Video" : "Show Video"}
+      </button>
+      <section>
+        {!canShowVideo ? null : canShowVideo && toggleVideo ? (
+          <video id="video" className="video-component" autoPlay></video>
+        ) : null}
+      </section>
+    </>
+  );
+};
 
 export default App;
