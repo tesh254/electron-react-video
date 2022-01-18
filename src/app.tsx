7870d7d2e01d6cc990a967d5c6bd2d37de2429a7
wachira_Dev <ewachira254@gmail.com>
2022-01-18 23:12:13 +0300

    @app/chore: setup react app with parcel

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC<{}> = () => {
+    return (
+        <section>
+            Hello this is react running on electron
+        </section>
+    )
+}
+
+export default App;
